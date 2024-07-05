507bea994c0c02de7803dd69908794b33172cb00
Sujeet Khadka <96431629+suj8@users.noreply.github.com>
2024-07-05 21:28:46 +0545

    updates

---
+++ Demo.java
@@ -0,0 +1,6 @@
+package example ;
+public class Demo {
+  public static void main(String[] a){
+    System.out.println("Hi");  
+  }  
+}
---
+++ Demo2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package example;
+
+/**
+ *
+ * @author Sujee
+ */
+public class Demo2 {
+public static void main(String[] a){
+    System.out.println("Hi this is demo2");  
+}    
+}
