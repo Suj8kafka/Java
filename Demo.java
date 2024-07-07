fbf5522c6f2db82dcb465585cb79a60945e64a96
Sujeet Khadka <96431629+suj8@users.noreply.github.com>
2024-07-07 22:55:54 +0545

    first commit

--- Demo.java
+++ Demo.java
@@ -1,6 +1,28 @@
-package example ;
-public class Demo {
-  public static void main(String[] a){
-    System.out.println("Hi");  
-  }  
+import java.util.*;
+
+class TestArray {
+    public static void main(String args[]) {
+        int a[][] = new int[3][3]; // declaration and instantiation
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter elements:");
+
+        // Input loop
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        // Print loop (moved outside the input loop)
+        System.out.println("----Entered Elements are ----");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println(); // Move to new line after each row
+        }
+        
+        sc.close(); // Close the scanner
+    }
 }
